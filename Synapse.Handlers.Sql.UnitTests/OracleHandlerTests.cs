b386c239de66485994af1f0c20aa8dcd8ab5430f
Siew Hooi Thong <sh_thong@outlook.com>
2019-05-06 09:42:34 +0800

    throws an exception to flag no unit test exists for OracleHandler

---
+++ Synapse.Handlers.Sql.UnitTests/OracleHandlerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synapse.Handlers.Sql.UnitTests
+{
+    [TestFixture]
+    public class OracleHandlerTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            throw new Exception( "There are no unit tests created for the OracleHandler" );
+        }
+    }
+}
